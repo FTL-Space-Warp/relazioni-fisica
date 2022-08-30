d84fca2bbee177bc00a05d0b20823df375135d29
Francesco Walhout <f.walhout@libero.it>
2022-08-30 16:10:45 +0200

    conteggi completata

--- conteggi/extra/conteggi.py
+++ conteggi/extra/conteggi.py
@@ -1,49 +1,87 @@
-import string
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import poisson, norm  # , binom
+from scipy.stats import poisson, norm, binom
 
 line_lengths = np.array([])
-letter_counts = {}
+letter_count = np.array([])
 
-for i in string.ascii_lowercase:
-    letter_counts[i] = []
 
 with open('dante.txt', 'r') as dante:
     for line in dante:
-        line = line.replace('\n', '').lower()
+        line = line.strip('\n ').lower()
         if line:
             line_lengths = np.append(line_lengths, len(line))
-            for i in string.ascii_lowercase:
-                letter_counts[i].append(line.count(i))
+            letter_count = np.append(letter_count, line.count("a"))
 
 N = len(line_lengths)
 m = line_lengths.mean()
 s = line_lengths.std(ddof=1)
-print(N)
-print(m)
-print(s)
+print(f"Numero di versi: {N}")
+print(f"Media campione delle lunghezze: {m}")
+print(f"Varianza campione delle lunghezze: {s}")
 
 binning = np.arange(line_lengths.min() - 0.5, line_lengths.max() + 1.5)
 
 plt.figure("Lunghezza dei versi")
-o, _, _ = plt.hist(line_lengths, bins=binning, rwidth=0.25, label="Conteggi")
+o, _, _ = plt.hist(line_lengths, bins=binning, label="Conteggi",
+                   histtype="step")
 plt.xlabel("Numero di caratteri per verso")
 plt.ylabel("Occorrenze")
+# print(o)
 
 k = np.arange(line_lengths.min(), line_lengths.max() + 1)
 e_poisson = N * poisson.pmf(k, m)
-e_gauss = N * norm.pdf(k, m, s)
-
-plt.bar(k - 0.3, e_poisson, width=0.25, color="#ff7f0e", label="Poisson")
-plt.plot(k, e_gauss, color="#2ca02c", label="Gauss")
+e_gauss = N * (norm.cdf(k+0.5, m, s) - norm.cdf(k-0.5, m, s))
 
 chi2_poisson = ((o - e_poisson)**2. / e_poisson).sum()
-chi2_gauss = ((o - e_gauss)**2. / e_gauss).sum()
 dof_poisson = len(k) - 1 - 1
+chi2_gauss = ((o - e_gauss)**2. / e_gauss).sum()
 dof_gauss = len(k) - 1 - 2
-print(f"chi2 per la Poissoniana: {chi2_poisson} / {dof_poisson} dof")
-print(f"chi2 per la Gaussiana: {chi2_gauss} / {dof_gauss} dof")
+
+plt.bar(k, e_poisson, width=0.25, color="#ff7f0e", label="Poisson")
+k = np.arange(line_lengths.min(), line_lengths.max() + 1, 0.1)
+plt.plot(k, N * norm.pdf(k, m, s), color="#2ca02c", label="Gauss")
+
+print(f"chi2 per la Poissoniana: {chi2_poisson} / {dof_poisson} dof \
+      (±{(2*dof_poisson)**.5})")
+print(f"chi2 per la Gaussiana: {chi2_gauss} / {dof_gauss} dof \
+      (±{(2*dof_gauss)**.5})")
 
 plt.legend()
+plt.savefig("lunghezze_versi.pdf")
+
+# Analisi delle occorrenze di una singola lettera
+
+p = letter_count.sum() / line_lengths.sum()
+
+binning = np.arange(letter_count.min() - 0.5, letter_count.max() + 1.5)
+
+plt.figure("Frequenza della lettera \"a\"")
+o, _, _ = plt.hist(letter_count, bins=binning, label="Conteggi",
+                   histtype="step")
+plt.xlabel("Occorrenze della lettera \"a\" per verso")
+plt.ylabel("Occorrenze")
+# print(o)
+
+k = np.arange(letter_count.min(), letter_count.max() + 1)
+e_binom = N * binom.pmf(k, m, p)
+e_poisson = N * poisson.pmf(k, p * m)
+
+chi2_binom = ((o - e_binom)**2 / e_binom).sum()
+dof_binom = len(k) - 1 - 2
+chi2_poisson = ((o - e_poisson)**2. / e_poisson).sum()
+dof_poisson = len(k) - 1 - 1
+
+plt.bar(k-0.125, e_poisson, width=0.25, color="#ff7f0e", label="Poisson")
+plt.bar(k+0.125, e_binom, width=0.25, color="green", label="Binomiale")
+
+
+print(f"chi2 per la Poissoniana: {chi2_poisson} / {dof_poisson} dof\
+      (±{(2*dof_poisson)**.5})")
+print(f"chi2 per la Binomiale: {chi2_binom} / {dof_binom} dof\
+      (±{(2*dof_binom)**.5})")
+
+plt.legend()
+plt.savefig("frequenza_a.pdf")
+
 plt.show()
